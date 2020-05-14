2ba9df64fc4b57c7364a4e0bb408d482f1636779
Ashik <mail@ashiknesin.com>
2020-05-14 10:20:38 +0530

    Add start script

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-let currentStepIndex = 0;
+let currentStepIndex = 1;
 
 const integSteps = [
     'INITIALIZATION',
     'CONNECT_QUOTE_TYPE',
     'CONNECT_SENDER_RULES',
     'CONNECT_SYNC_RULES',
-    'COMPLETED'
+    'COMPLETED',
 ];
 
 let payload = {
@@ -40,23 +40,23 @@ let payload = {
         isDraft: false,
         isOverridable: true,
         value: null,
-        searchable:true,
+        searchable: true,
         options: [
             {
                 displayName: 'Quotes Template 1',
-                description: "Quotes / Quotes Template 1",
-                value: 'qt1'
+                description: 'Quotes / Quotes Template 1',
+                value: 'qt1',
             },
             {
                 displayName: 'Quotes Template 2',
-                description: "Quotes / Quotes Template 2",
-                value: 'qt2'
+                description: 'Quotes / Quotes Template 2',
+                value: 'qt2',
             },
             {
                 displayName: 'Quotes Template X',
-                description: "Quotes / Quotes Template x",
-                value: 'qtx'
-            }
+                description: 'Quotes / Quotes Template x',
+                value: 'qtx',
+            },
         ],
     },
     isAttachChargebeeQuote: {
@@ -68,23 +68,23 @@ let payload = {
         isDraft: false,
         isOverridable: true,
         value: null,
-        searchable:true,
+        searchable: true,
         options: [
             {
                 displayName: 'Ashik Nesin',
-                description: "mail@ashiknesin.com",
-                value: 'an'
+                description: 'mail@ashiknesin.com',
+                value: 'an',
             },
             {
                 displayName: 'Quotes Template 2',
-                description: "Quotes / Quotes Template 2",
-                value: 'qt2'
+                description: 'Quotes / Quotes Template 2',
+                value: 'qt2',
             },
             {
                 displayName: 'Quotes Template X',
-                description: "Quotes / Quotes Template x",
-                value: 'qtx'
-            }
+                description: 'Quotes / Quotes Template x',
+                value: 'qtx',
+            },
         ],
     },
     isAllowOnlyDefaultSender: {
@@ -101,39 +101,38 @@ let payload = {
         isDraft: false,
         value: false,
         isOverridable: true,
-
     },
     isFeatureAllowed: false,
     isUnlinkAllowed: true,
     dashboardOverview: {
         connectedOn: 1589214997720,
     },
-    allowProceed:true,
-    proceedBtnTooltip:'You must map all GetAccept fields to Chargebee quote fields to proceed.',
-    isDraft:false,
-    errorAlerts: ['TEMPLATE_UNAVAILABLE','SENDER_UNAVAILABLE'],
+    allowProceed: true,
+    proceedBtnTooltip:
+        'You must map all GetAccept fields to Chargebee quote fields to proceed.',
+    isDraft: false,
+    errorAlerts: ['TEMPLATE_UNAVAILABLE', 'SENDER_UNAVAILABLE'],
     getAcceptFieldsOption: {
         isDraft: false,
         options: [
             {
                 displayName: 'Customer Company',
                 value: 'ga_cc',
-                cbQuoteFieldValue:"cb_rc",
-
+                cbQuoteFieldValue: 'cb_rc',
             },
             {
                 displayName: 'Recipient First Name',
                 value: 'ga_rfa',
-                cbQuoteFieldValue:null
+                cbQuoteFieldValue: null,
             },
             {
                 displayName: 'Document Number',
                 value: 'ga_dn',
-                cbQuoteFieldValue:null
-            }
+                cbQuoteFieldValue: null,
+            },
         ],
     },
-    cbQuoteFieldsOption:{
+    cbQuoteFieldsOption: {
         options: [
             {
                 displayName: 'Recipient Company',
@@ -141,14 +140,90 @@ let payload = {
             },
             {
                 displayName: 'Customer First Name',
-                value: 'cb_cfn'
+                value: 'cb_cfn',
             },
             {
                 displayName: 'Quote ID',
-                value: 'cb_qi'
-            }
+                value: 'cb_qi',
+            },
         ],
-    }
+    },
+};
+
+const sendViaGetAcceptPayload = {
+    quoteTypeOption: {
+        value: 'DETAILED',
+        options: [
+            {
+                displayName: 'Consolidated',
+                description:
+                    'Send quotes with the summary of all charges applicable through the lifetime of the subscription.',
+                value: 'CONSOLIDATED',
+                tag: 'Default',
+                hideHeading: 'hideHeading',
+            },
+            {
+                displayName: 'Detailed',
+                value: 'DETAILED',
+                description:
+                    'Send quotes with a detailed view of the charges applicable for every billing cycle through the lifetime of the subscription.',
+            },
+        ],
+    },
+    templateName: 'Proposal Document Jan-2020',
+    senderOption: {
+        value: 'rn',
+        searchable: true,
+        options: [
+            {
+                displayName: 'Ashik Nesin',
+                email: 'mail@ashiknesin.com',
+                value: 'an',
+            },
+            {
+                displayName: 'RN',
+                email: 'rn@nesin.io',
+                value: 'rn',
+            },
+        ],
+    },
+    isRestrictedSender: false,
+    actionOnSignOption: {
+        value: 'MARK_QUOTE_AND_CREATE_SUB',
+        options: [
+            {
+                displayName: 'Mark quote as ‘Accepted’',
+                description:
+                    'Once a quote is signed, it will be marked as ‘Accepted’ in Chargebee. You need to manually create a subscription for the quote.',
+                value: 'MARK_QUOTE_AS_ACCEPTED',
+            },
+            {
+                displayName:
+                    'Mark quote as ‘Accepted’ and create a subscription in Chargebee',
+                description:
+                    "Once a quote is signed. it will be marked as 'Accepted' and a subscription will be automatically created in Chargebee, with auto-collection turned off.",
+                value: 'MARK_QUOTE_AND_CREATE_SUB',
+            },
+        ],
+    },
+    roleOption: {
+        options: [
+            {
+                displayName: `Owner 'Signer'`,
+                value: 'owner_signer',
+            },
+            {
+                displayName: `Finance Controller 'Approver'`,
+                value: 'finance_controller_approver',
+            },
+            {
+                displayName: `Partner 'Only view'`,
+                value: 'partner_only_view',
+            },
+        ],
+    },
+    allowProceed: true,
+    errorAlerts: ['TEMPLATE_UNAVAILABLE'],
 };
 
 /* home route */
@@ -160,6 +235,10 @@ app.get('/third_party/ui/getaccept', (req, res) => {
     res.send({ ...payload, step: integSteps[currentStepIndex] });
 });
 
+app.get('/getaccept/send_via_get_accept', (req, res) => {
+    res.send(sendViaGetAcceptPayload);
+});
+
 app.post('/third_party/ui/getaccept/stage', (req, res) => {
     currentStepIndex += 1;
     res.send({ ...payload, step: integSteps[currentStepIndex] });
@@ -168,21 +247,23 @@ app.post('/third_party/ui/getaccept/stage', (req, res) => {
 app.post('/third_party/ui/getaccept/save', (req, res) => {
     payload = {
         ...payload,
-        [req.body.prefName]:{
+        [req.body.prefName]: {
             ...payload[req.body.prefName],
-            value:req.body.value,
-            isDraft:false,
-            isOverridable:true
-        }
-    }
+            value: req.body.value,
+            isDraft: false,
+            isOverridable: true,
+        },
+    };
 
-    if(req.body.prefName==="cbGAFieldMapper"){
-        payload.getAcceptFieldsOption.options = payload.getAcceptFieldsOption.options.map(item => {
-            if(item.value === req.body.value.getAcceptFieldValue){
-                item.cbQuoteFieldValue = req.body.value.cbQuoteFieldValue
+    if (req.body.prefName === 'cbGAFieldMapper') {
+        payload.getAcceptFieldsOption.options = payload.getAcceptFieldsOption.options.map(
+            item => {
+                if (item.value === req.body.value.getAcceptFieldValue) {
+                    item.cbQuoteFieldValue = req.body.value.cbQuoteFieldValue;
+                }
+                return item;
             }
-            return item;
-        })
+        );
     }
     res.send({ ...payload, step: integSteps[currentStepIndex] });
 });
